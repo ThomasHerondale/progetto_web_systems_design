07b895b5363b1fdd7ae6410736523af1a3a20150
thomas <thomasherondale@gmail.com>
2024-03-16 17:20:19 +0100

    Initial commit

---
+++ src/main/java/it/wsda/WebSystemsDesignProjectApplication.java
@@ -0,0 +1,13 @@
+package it.wsda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSystemsDesignProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSystemsDesignProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/wsda/WebSystemsDesignProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.wsda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSystemsDesignProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
