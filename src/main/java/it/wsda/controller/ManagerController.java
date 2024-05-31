1d7b1785431a7433dae880b54bca95e02a81311e
ciposs <federicocpl@outlook.it>
2024-05-31 17:56:47 +0200

    Creazione delle stile e di manager.html

--- src/main/java/it/wsda/controller/ManagerController.java
+++ src/main/java/it/wsda/controller/ManagerController.java
@@ -1,18 +1,27 @@
 package it.wsda.controller;
 
 
+import it.wsda.entity.Facility;
+import it.wsda.entity.Signal;
 import it.wsda.services.FacilitiesService;
+import it.wsda.services.SchedulesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class ManagerController {
 
     private final FacilitiesService facilitiesService;
+    private final SchedulesService schedulesService;
 
-    public ManagerController(FacilitiesService facilitiesService) {
+    public ManagerController(FacilitiesService facilitiesService, SchedulesService schedulesService) {
+        this.schedulesService = schedulesService;
         this.facilitiesService = facilitiesService;
     }
 
@@ -20,6 +29,15 @@ public class ManagerController {
     public String home(Model model) {
         var facilities = facilitiesService.getAllFacilities();
         model.addAttribute("facilities", facilities);
+
+        var schedules = schedulesService.getAllSchedules();
+        model.addAttribute("schedules", schedules);
         return "manager";
     }
+
+    @PostMapping("update")
+    public String updateFacility() {
+
+    }
+
 }
