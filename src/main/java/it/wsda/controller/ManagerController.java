bb0b618555c571ee40edde010eb6720aba8d3f9f
thomas <thomasherondale@gmail.com>
2024-05-31 11:11:54 +0200

    manager controller

--- src/main/java/it/wsda/controller/ManagerController.java
+++ src/main/java/it/wsda/controller/ManagerController.java
@@ -1,14 +1,25 @@
 package it.wsda.controller;
 
 
+import it.wsda.services.FacilitiesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class ManagerController {
 
+    private final FacilitiesService facilitiesService;
+
+    public ManagerController(FacilitiesService facilitiesService) {
+        this.facilitiesService = facilitiesService;
+    }
+
     @GetMapping("/manager")
-    public String home() {
+    public String home(Model model) {
+        var facilities = facilitiesService.getAllFacilities();
+        model.addAttribute("facilities", facilities);
         return "manager";
     }
 }
