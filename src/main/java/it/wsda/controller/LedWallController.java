c7517f507a8becf39c44bbc5773bd350c997791e
Haydnz <96525793+Haydnz@users.noreply.github.com>
2024-06-02 19:06:48 +0200

    LedWallController

---
+++ src/main/java/it/wsda/controller/LedWallController.java
@@ -0,0 +1,16 @@
+package it.wsda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LedWallController {
+
+    @GetMapping("/wsda")
+    public String showLedWall(@RequestParam int id, Model model) {
+        model.addAttribute("facilityId", id);
+        return "led_wall";
+    }
+}
