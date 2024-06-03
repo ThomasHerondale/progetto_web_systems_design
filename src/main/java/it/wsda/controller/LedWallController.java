79125e60cdcdc1c714591c4026cffae7a9a0579c
thomas <thomasherondale@gmail.com>
2024-06-03 16:50:19 +0200

    inactiveFacility controller

--- src/main/java/it/wsda/controller/LedWallController.java
+++ src/main/java/it/wsda/controller/LedWallController.java
@@ -1,5 +1,6 @@
 package it.wsda.controller;
 
+import it.wsda.services.FacilitiesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LedWallController {
 
+    private final FacilitiesService facilitiesService;
+
+    public LedWallController(FacilitiesService facilitiesService) {
+        this.facilitiesService = facilitiesService;
+    }
+
     @GetMapping("/wsda")
     public String showLedWall(@RequestParam int id, Model model) {
         model.addAttribute("facilityId", id);
 
-        return "led_wall";
+        var facility = facilitiesService.findFacilityById(id);
+        if (facility.getStatus().equals("INACTIVE"))
+            return "inactiveFacility";
+        else
+            return "led_wall";
     }
 }
